e00af4a671bb91117dac65d29cdb7c72a6d770e7
cnpatton <botanicality@gmail.com>
2019-02-26 19:13:37 -0600

    Adding better movements and shooting controls

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -11,6 +11,8 @@ public class Ship : MonoBehaviour {
     private float _fireTimer;
     private float _fireCooldown = 0.5f;
 
+    private Vector2 DirectionalInput;
+
     // Use this for initialization
     void Start () {
         _parentMover = transform.parent;
@@ -18,21 +20,42 @@ public class Ship : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        _parentMover.position += transform.forward * Speed * Time.deltaTime;
+        Move();
+        CheckForFire();
+        //_fireTimer -= Time.deltaTime;
+        //if(_fireTimer <= 0) {
+        //    _fireTimer = _fireCooldown;
 
-        _fireTimer -= Time.deltaTime;
-        if(_fireTimer <= 0) {
-            _fireTimer = _fireCooldown;
+        //    //RaycastHit hit;
+        //    if(Input.GetKeyDown(KeyCode.Space)) {//if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+        //        //print("Hit object : " + hit.transform.tag);
+        //        //if (hit.transform.tag == "Damageable") {
+        //            FireBullet();
+        //        //}
+        //    }
+        //}
+    }
 
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit)) {
-                print("Hit object : " + hit.transform.tag);
-                if (hit.transform.tag == "Damageable") {
-                    FireBullet();
-                }
-            }
+    private void GetPlayerInput() {
+        DirectionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        print("Player Input : " + DirectionalInput);
+    }
+
+    private void Move() {
+        GetPlayerInput();
+
+        var adjustedSpeed = Speed * (DirectionalInput.y > 0 ? 2f : DirectionalInput.y < 0 ? 0.5f : 1f);
+        _parentMover.position += transform.forward * adjustedSpeed * Time.deltaTime;
+
+        print("adjustedSpeed: " + adjustedSpeed);
+    }
+
+    private void CheckForFire() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            FireBullet();
         }
-	}
+    }
+
     private void FireBullet() {
         var clone = Instantiate(BulletPrefab, transform.position, transform.parent.rotation) as Transform;
         var bulletScript = clone.GetComponent<Bullet>();
